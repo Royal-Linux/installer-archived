c36d9397344dfb5354006dc4afa7b2a774c59049
ulises-jeremias <ulisescf.24@gmail.com>
2021-01-31 01:55:49 -0300

    Updated version

--- profiledef.sh
+++ profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="archroyal"
-iso_label="ARCHROYAL021"
+iso_label="ARCHROYAL022"
 iso_publisher="ArchRoyal Installer <https://github.com/ArchRoyal-Linux/installer>"
 iso_application="ArchRoyal Installer"
-iso_version="0.2.1"
+iso_version="0.2.2"
 install_dir="archroyal"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
