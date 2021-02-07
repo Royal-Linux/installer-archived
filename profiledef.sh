be5c0108749ea7352197ddb0c0cc6f23e26419a5
ulises-jeremias <ulisescf.24@gmail.com>
2021-02-07 00:15:33 -0300

    Updated to fix partitioning

--- profiledef.sh
+++ profiledef.sh
@@ -10,3 +10,12 @@ install_dir="archroyal"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/bin/archroyal"]="0:0:755"
+)
